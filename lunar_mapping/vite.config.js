4f4b15b1c2710af325e57d6da827a7c93c5276a0
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-12-11 21:03:33 +0530

    updated

--- lunar_mapping/vite.config.js
+++ lunar_mapping/vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [visualizer()],
-});
+export default {
+  plugins: [react()],
+};
